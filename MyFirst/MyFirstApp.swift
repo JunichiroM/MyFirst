800f612294da4dba435dfcdd2ac294060b64b35a
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-09-14 11:25:51 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by apple on 2024/09/14.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
